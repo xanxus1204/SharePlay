81dd55911dc01b3ddc75aa3633b2d34b7a4a39a1
椛島優 <icloud_i4s@me.com>
2016-04-15 16:36:45 +0900

    はじめてのコミット
    Swiftは難しい

---
+++ SharePlay/FirstViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  SharePlay
+//
+//  Created by 椛島優 on 2016/04/12.
+//  Copyright © 2016年 椛島優. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    var  multiPeer :MultiPeerNetwork!
+    var roomName:String?
+    @IBOutlet weak var nameText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        multiPeer = MultiPeerNetwork()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func createButtonTapped(sender: AnyObject) {
+        if let roomName = self.nameText.text {
+            multiPeer.startServerWithName(roomName)
+        }
+        self.segueFirstToSecond()
+        
+        
+    }
+
+    @IBAction func searchButtonTapped(sender: AnyObject) {
+        if let roomName = self.nameText.text {
+            multiPeer.startClientWithName(roomName)
+        }
+        self.segueFirstToSecond()
+    }
+    
+    func segueFirstToSecond(){
+        //画面遷移処理
+        if let nextViewController = storyboard?.instantiateViewControllerWithIdentifier("second") as? SecondViewController{
+            
+            self.presentViewController(nextViewController,animated: true,completion: nil)
+        }
+
+    }
+}
+
---
+++ SharePlay/ThirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdViewController.swift
+//  SharePlay
+//
+//  Created by 椛島優 on 2016/04/12.
+//  Copyright © 2016年 椛島優. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
